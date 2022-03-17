9da314e16c38c26967c66da730dd1536fa19ddda
yashmittal2 <yashmittal2@deloitte.com>
2022-03-17 16:32:37 +0530

    Reverse done

--- Collections/src/ReverseOrder.java
+++ Collections/src/ReverseOrder.java
@@ -4,16 +4,16 @@ public class ReverseOrder {
     public static void main(String[] args) {
         Scanner scan = new Scanner(System.in);
         System.out.println("Enter size of list:");
-        int n = scan.nextInt() + 1;
-        String[] str = new String[n];
+        int num = scan.nextInt() + 1;
+        String[] str = new String[num];
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num; i++) {
             System.out.print("Enter Element of list:");
             str[i] = scan.nextLine();
         }
         Stack<String> stack = new Stack<>();
 
-        for (int i = 0; i < n; i++) {
+        for (int i = 0; i < num; i++) {
             stack.push(str[i]);
         }
 
